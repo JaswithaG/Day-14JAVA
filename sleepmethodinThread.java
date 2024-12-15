9abe808f9859b22678ea3ebf90f6ac8db9c47075
JaswithaG <gummijaswitha@gmail.com>
2024-12-15 11:09:41 +0530

    Add files via upload

---
+++ sleepmethodinThread.java
@@ -0,0 +1,14 @@
+public class sleepmethodinThread{
+    public static void main(String [] args){
+        System.out.println("Thread started.......");
+        try 
+        {
+            Thread.sleep(10000);
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println("Interrupted");
+        }
+        System.out.println("Thread Resumed");
+    }
+}
